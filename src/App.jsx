f2c133c7b9a91e68baac965c8aaee2a73cb59b45
sqMax2 <msvp@mail.ru>
2024-03-03 16:05:19 +0300

    feat: Adds form for developing custom components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import "./App.css";
 
 function App() {
+  const formRef = useRef(null);
+  const [signupData, setSignupData] = useState({});
 
+  const handleChange = (e) => {
+    setSignupData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    formRef.current.reset();
+  };
+
+  const handleReset = (e) => {
+    setSignupData({});
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-
+          <form
+            ref={formRef}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            onReset={handleReset}
+          >
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </header>
     </div>
