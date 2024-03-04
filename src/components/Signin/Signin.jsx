40cea784bac397b58da841c64468fac78467af02
Sergei <msvp@mail.ru>
2024-03-04 19:08:30 +0300

    feat: Adds blanc Signin and Signup components

---
+++ src/components/Signin/Signin.jsx
@@ -0,0 +1,13 @@
+import { TextInput } from "../TextInput/TextInput";
+
+export function Signin(props) {
+	const {
+		onSubmit,
+	} = props;
+
+	return (
+		<div>
+			<TextInput />
+			Signin</div>
+	);
+}
---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,13 @@
+import { TextInput } from "../TextInput/TextInput";
+
+export function Signup(props) {
+	const {
+		onSubmit,
+	} = props;
+
+	return (
+		<div>
+			<TextInput />
+			Signup</div>
+	);
+}
