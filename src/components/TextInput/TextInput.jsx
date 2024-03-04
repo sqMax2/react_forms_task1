d76128f44bf26e485a00df1f6d7794b79e718baf
Sergei <msvp@mail.ru>
2024-03-04 18:46:30 +0300

    docs: Adds TextInput doc

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,7 +1,23 @@
 import { useEffect, useId, useRef } from "react";
 import "../../styles/TextInput.css";
 
-
+/**
+ * Text input component.
+ *
+ * @param {object} props - The props object containing:
+ * label: string - The label text.
+ * description: string - The description text.
+ * error: string - The error text.
+ * withAsterisk: boolean - Whether to display an asterisk after the label.
+ * icon: string - The icon to display before the input.
+ * variant: "filled" | "unstylled" | null - The variant of the input.
+ * radius: "rsm" | "rmd" | "rlg" | "rxl" - The border radius of the input.
+ * size: "xs" | "sm" | "md" | "lg" | "xl" - The size of the input.
+ * isEmail: boolean - Whether the input is an email input.
+ * ref: React.RefObject<HTMLInputElement> - The reference to the input element.
+ * rest: any - The rest of the HTML input element props.
+ * @return {JSX.Element} The rendered text input component.
+ */
 export function TextInput(props) {
   const id = useId();
   const blockRef = useRef(null);
