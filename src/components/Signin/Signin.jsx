66e34823fbb6d0b6df9d542cd8cee2727115dd07
Sergei <msvp@mail.ru>
2024-03-05 19:20:02 +0300

    feat: Implements Signin component

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { TextInput } from "../TextInput/TextInput";
 
 export function Signin(props) {
-	const {
-		onSubmit,
-	} = props;
+  const [signinData, setSigninData] = useState({});
+  const { onSubmit, ...rest } = props;
 
-	return (
-		<div>
-			<TextInput />
-			Signin</div>
-	);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(signinData);
+  };
+
+  const handleChange = (e) => {
+    setSigninData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleReset = (e) => {
+    setSigninData({});
+  };
+
+  return (
+    <form onSubmit={handleSubmit} onChange={handleChange} {...rest}>
+      <TextInput name="email" isEmail />
+      <TextInput name="password" isPassword />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
