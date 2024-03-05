ae7e4de2c72b334501d059a14c115ca77b2f5026
Sergei <msvp@mail.ru>
2024-03-05 19:22:30 +0300

    feat: Adds Signin and Signup components into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
 import { useRef, useState } from "react";
 import "./App.css";
 import { TextInput } from "./components/TextInput/TextInput";
+import { Signin } from "./components/Signin/Signin";
+import { Signup } from "./components/Signup/Signup";
 
 function App() {
   const formRef = useRef(null);
+  const [signinData, setSigninData] = useState({});
   const [signupData, setSignupData] = useState({});
 
-  const handleChange = (e) => {
-    setSignupData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }))
+  const handleSigninSubmit = (data) => {
+    setSigninData(data);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    formRef.current.reset();
-  };
-
-  const handleReset = (e) => {
-    setSignupData({});
+  const handleSignupSubmit = (data) => {
+    setSignupData(data);
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <form className="dark-theme"
-            ref={formRef}
-            onChange={handleChange}
-            onSubmit={handleSubmit}
-            onReset={handleReset}
-          >
-            <TextInput label="Smallest" size="xs" placeholder="Placeholder" withAsterisk/>
-            <TextInput size="sm" label="Small" description="This is a description" radius="rmd" error="This is an error"/>
-            <TextInput size="md" label="Medium" radius="rxl" variant="unstylled" icon="@"/>
-            <TextInput size="lg" label="Large" description="This is a description" variant="filled" isEmail/>
-            <TextInput size="xl" label="Largest" radius="rmd" />
-
-
-            <button type="submit">Submit</button>
-          </form>
+          <Signin className="dark-theme" onSubmit={handleSigninSubmit} />
+          <div>
+            {Object.keys(signinData).length !== 0 ? (
+              <>
+                <p>email: {signinData.email}</p>
+                <p>password: {signinData.password}</p>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+        <div>
+          <Signup className="dark-theme" onSubmit={handleSignupSubmit} />
+          <div></div>
         </div>
       </header>
     </div>
