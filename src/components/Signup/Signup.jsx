14304932e7c7663fd8ee57b71bf1831ac7cc2a8c
Sergei <msvp@mail.ru>
2024-03-05 19:23:11 +0300

    feat: Adds Signup component basic layout

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,13 +1,64 @@
 import { TextInput } from "../TextInput/TextInput";
+import { useState } from "react";
 
 export function Signup(props) {
-	const {
-		onSubmit,
-	} = props;
+  const [signupData, setSignupData] = useState({});
+  const [gender, setGender] = useState("");
+  const { onSubmit, ...rest } = props;
 
-	return (
-		<div>
-			<TextInput />
-			Signup</div>
-	);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(signupData);
+  };
+
+  const handleChange = (e) => {
+    setSignupData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleReset = (e) => {
+    setSignupData({});
+  };
+
+  const handleGender = (e) => {
+    setGender(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} onChange={handleChange} {...rest}>
+      <TextInput name="name" label="Name" />
+      <TextInput name="nickname" label="Nickname" />
+      <TextInput name="email" isEmail label="Email" />
+      <fieldset>
+        <legend>Gender:</legend>
+        <div>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            id="gender-male"
+            checked={gender === "male"}
+            onChange={handleGender}
+          />
+          <label htmlFor="gender-male">male</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            id="gender-female"
+            checked={gender === "female"}
+            onChange={handleGender}
+          />
+          <label htmlFor="gender-female">female</label>
+        </div>
+      </fieldset>
+      <TextInput name="password" isPassword label="Password" />
+      <TextInput name="repeatPassword" isPassword label="Repeat Password" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
